f2c2b7c1abed11ee9150c998ddf68df8f6844400
Aexandre Leclercq <41374291+AlexLecq@users.noreply.github.com>
2021-05-25 16:29:03 +0200

    update

--- setup-windows.ps1
+++ setup-windows.ps1
@@ -1,2 +1,41 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
+
+function Set-WallPaper {
+    param(
+        [string]$PathImage
+    )
+    Write-Host "-------------- Set Wallpaper -------------";
+    set-itemproperty -path "HKCU:Control Panel\Desktop" -name WallPaper -value  (Join-Path -Path $pwd -ChildPath $PathImage)
+    RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
+}
+
+function Disable-Cortana {
+    Write-Host "-------------- Disable Cortana -------------";
+    $path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"; 
+    IF(!(Test-Path -Path $path)) { 
+        New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows" -Name "Windows Search";
+    } 
+    Set-ItemProperty -Path $path -Name "AllowCortana" -Value 0;
+    #Restart Explorer to change it immediately    
+    Stop-Process -name explorer;
+}
+
+function Install-Choco{
+    Write-Host "-------------- Install Chocolatey -------------";
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
+    Try {
+        choco -v
+    } Catch {
+        Write-Host "choco is not install";
+        exit;
+    }
+}
+
+Set-ExecutionPolicy UnRestricted -Force;
+
+Install-Choco;
+Write-Host "-------------- Apps installing -------------";
 choco install firefox winrar avgantivirusfree ccleaner libreoffice-fresh filezilla vlc jre8 -y;
+Disable-Cortana;
+Set-Wallpaper -PathImage "assets\wallpaper.jpg";
+
+Set-ExecutionPolicy Restricted -Force;
